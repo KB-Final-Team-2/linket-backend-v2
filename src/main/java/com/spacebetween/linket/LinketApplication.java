4d75b6cbdcd52ad089a6b83694eac782f79b87c4
koojiny <msj09252@gmail.com>
2023-07-24 12:25:12 +0900

    first commit

---
+++ src/main/java/com/spacebetween/linket/LinketApplication.java
@@ -0,0 +1,13 @@
+package com.spacebetween.linket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spacebetween/linket/LinketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spacebetween.linket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
